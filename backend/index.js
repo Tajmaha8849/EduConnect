fa19f03add09dec7efa93a23fe1e9fe4e021cad4
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-12-31 10:37:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
+//import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
@@ -43,7 +43,7 @@ app.get('/health', (req, res) => {
 });
 
 // API routes
-app.use('/api/user', userRouter);
+//app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
