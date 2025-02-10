d4057a5a1d4de1d1f1b42d0d7916bae921f684e1
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2025-02-10 20:49:40 +0530

    Update classroomRoutes.js

--- backend/routes/classroomRoutes.js
+++ backend/routes/classroomRoutes.js
@@ -17,7 +17,7 @@ const mailer = async (receiverEmail, code) => {
     requireTLS: true,
     auth: {
         user: "shubhamprajapati9537@gmail.com", // Your Gmail address
-        pass: "jgxkmkvlncvmsqwk",  // Your App Password
+        pass: "atexcqhnyrmakewx",  // Your App Password
     },
 });
 
